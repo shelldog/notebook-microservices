7ace8399c64952e310f76120005d523914ed3622
shelldog <hajimedevlife@gmail.com>
2022-05-10 05:34:20 -0400

    add delete service & fix the UI!

--- notebook/src/components/App.jsx
+++ notebook/src/components/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
 
     if (content.length < 50) {
       const { data } = await axios.post(
-        `http://${process.env.NOTEBOOK_ROUTE}/${process.env.NOTE_SERVICE_ROUTE}/api/route`,
+        `http://${process.env.NOTEBOOK_ROUTE}/${process.env.NOTE_SERVICE_ROUTE}/api/note`,
         {
           content: input.current.value,
           status: 'pending',
@@ -29,7 +29,7 @@ export default function App() {
 
   const fetchNotes = async () => {
     const { data } = await axios.get(
-      `http://${process.env.NOTEBOOK_ROUTE}/${process.env.QUERY_SERVICE_ROUTE}}/api/queries`,
+      `http://${process.env.NOTEBOOK_ROUTE}/${process.env.QUERY_SERVICE_ROUTE}/api/queries`,
     )
 
     setNotes(data.data)
